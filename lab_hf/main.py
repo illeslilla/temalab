b2c15c37e287278d3b288c18eeead3467521d6b8
illeslilla <hoshishiro.1>
2019-11-23 18:56:20 +0100

    Train

---
+++ lab_hf/main.py
@@ -0,0 +1,89 @@
+import numpy as np
+import string
+import os
+import re
+import random
+import cv2
+import pickle
+
+DATADIR ="English\Img\Bmp"
+CLASSES = string.ascii_uppercase
+IMG_SIZE = 64
+
+
+def load_filenames():
+    filenames = []
+    for path, dirs,files in os.walk(DATADIR):
+        """Fájlrendszer bejárása és kép elérési utak betöltése"""
+        filenames += list(map(lambda f: path+'\\'+f, files))
+    return filenames
+
+
+def create_datasets(dataset):
+    test_img = []
+    train_img = []
+
+    """fájlok megkeverse és szétosztása két állományba"""
+    random.shuffle(dataset)
+    j=0;
+    for i in dataset:
+        if j<len(dataset)*0.7:
+            train_img.append(i)
+        else:
+            test_img.append(i)
+        j+=1;
+    return train_img, test_img
+
+
+def get_class(filename):
+    """Mappaszámot visszalakíom karakterré hogy megtudjam a kép milyen karaktert ábrázol"""
+    return CLASSES[int(re.findall(r'.*Sample(\d+).*', filename)[0])-11]
+
+
+def open_img (filename):
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) * cv2.imread(filename.replace('Bmp', 'Msk'), cv2.IMREAD_GRAYSCALE)
+    processed_img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+    return processed_img
+
+
+def get_dataset(dataset):
+    data = []
+    for i in dataset:
+        img = open_img(i)
+        lab = get_class(i)
+        data.append([img, lab])
+    return data
+
+def save_dataset(data, img_name, lab_name):
+    X = []
+    Y = []
+
+    for img, lab in data:
+        X.append(img)
+        Y.append(lab)
+
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    pickle_out = open(img_name, "wb")
+    pickle.dump(X, pickle_out)
+    pickle_out.close()
+
+    pickle_out = open(lab_name, "wb")
+    pickle.dump(Y, pickle_out)
+    pickle_out.close()
+
+
+if __name__ == "__main__":
+    filenames = load_filenames()
+    train_img, test_img = create_datasets(filenames)
+    train_data = get_dataset(train_img)
+    test_data = get_dataset(test_img)
+
+    save_dataset(train_data, "train_images", "train_labels")
+    save_dataset(test_data, "test_images", "test_labels")
+
+
+
+
+
+
+
---
+++ lab_hf/training.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import pickle
+
+
+
+if __name__ == "__main__":
+    train_img = pickle.load(open("train_images", "rb"))
+    train_lab = pickle.load(open("train_labels", "rb"))
+
+    train_img= train_img/255.0
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D((64), (3, 3), input_shape = train_img.shape[1:]))
+    model.add(tf.keras.layers.Activation("relu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(tf.keras.layers.Conv2D((64), (3, 3)))
+    model.add(tf.keras.layers.Activation("relu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64))
+    #model.add(tf.keras.layers.Activation("relu"))
+
+    model.add(tf.keras.layers.Dense(26))
+    model.add(tf.keras.layers.Activation("sigmoid"))
+
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.fit(train_img, train_lab, batch_size=26, epochs=3, validation_split=0.1)
+
